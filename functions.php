2e415574137200ef9643797cae4b81e1a2392b1d
Devin <devinvinson@gmail.com>
2014-05-13 11:37:38 -0400

    Functions cleanup
    Removed functions that aren’t really default and uncommented everything
    else. Should be a better one-step installation.

--- functions.php
+++ functions.php
@@ -32,23 +32,4 @@ function avia_include_shortcode_template($paths)
  *	Turn on Custom CSS Class field for all Avia Layout Builder elements
  *	@link http://www.kriesi.at/documentation/enfold/turn-on-custom-css-field-for-all-alb-elements/
  */
-	//add_theme_support('avia_template_builder_custom_css');
-
-/**
- *	Remove the Import dummy data button from theme options
- *	@link http://www.kriesi.at/documentation/enfold/remove-the-import-dummy-data-button/
- */
-	//add_theme_support('avia_disable_dummy_import');
-
-/**
- *	Enable Avia Layout Builder Debug
- *	@link http://www.kriesi.at/documentation/enfold/enable-advanced-layout-builder-debug/
- */
-
-	/*
-	add_action('avia_builder_mode', "builder_set_debug");
-	function builder_set_debug() {
-		return "debug";
-	}
-	*/
-
+	add_theme_support('avia_template_builder_custom_css');
